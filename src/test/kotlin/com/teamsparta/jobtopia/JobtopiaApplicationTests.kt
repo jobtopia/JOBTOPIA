1a1d0d516277a9ece4ff994aadace70746d1f644
DanDanjoo <murekal94@gmail.com>
2024-06-11 17:16:27 +0900

    :heavy_plus_sign: #2 - 의존성 주입 및 프로젝트 초기 셋팅

---
+++ src/test/kotlin/com/teamsparta/jobtopia/JobtopiaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teamsparta.jobtopia
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JobtopiaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
