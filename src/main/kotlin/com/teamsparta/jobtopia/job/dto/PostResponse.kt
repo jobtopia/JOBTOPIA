9d0e3a010109431bef35fc7eb6a306fe8a3f4614
gooddle <vptl6559@gmail.com>
2024-06-12 10:40:08 +0900

    :sparkles: post dto 추가

---
+++ src/main/kotlin/com/teamsparta/jobtopia/job/dto/PostResponse.kt
@@ -0,0 +1,11 @@
+package com.teamsparta.jobtopia.post.dto
+
+import java.time.LocalDateTime
+
+data class PostResponse (
+    val id : Long,
+    val title : String,
+    val content : String,
+    val createdAt : LocalDateTime,
+    val updatedAt  : LocalDateTime,
+)
