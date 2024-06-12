aa93cc24715a9884504603e0b55d1ed745300001
gooddle <vptl6559@gmail.com>
2024-06-12 19:10:30 +0900

    :zap: #20 패키치 위치 이동 및 게시글 업데이트 반환 형식 수정

--- src/main/kotlin/com/teamsparta/jobtopia/domain/post/service/PostService.kt
+++ src/main/kotlin/com/teamsparta/jobtopia/domain/post/service/PostService.kt
@@ -1,7 +1,8 @@
-package com.teamsparta.jobtopia.post.service
+package com.teamsparta.jobtopia.domain.post.service
 
-import com.teamsparta.jobtopia.post.dto.PostRequest
-import com.teamsparta.jobtopia.post.dto.PostResponse
+import com.teamsparta.jobtopia.domain.post.dto.PostRequest
+import com.teamsparta.jobtopia.domain.post.dto.PostResponse
+import com.teamsparta.jobtopia.domain.post.dto.UpdatePostResponse
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 
@@ -11,6 +12,6 @@ interface PostService {
     fun getPostList(pageable: Pageable): Page<PostResponse>
     fun getPostById(postId: Long):PostResponse
     fun createPost(postRequest: PostRequest):PostResponse
-    fun updatePost(postId: Long,postRequest: PostRequest):PostResponse
+    fun updatePost(postId: Long,postRequest: PostRequest):UpdatePostResponse
     fun deletePost(postId: Long)
 }
