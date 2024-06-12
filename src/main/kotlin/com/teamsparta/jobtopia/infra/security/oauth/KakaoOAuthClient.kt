e19eab0e92678833bfcfa9b3db412943ffad3536
DanDanjoo <murekal94@gmail.com>
2024-06-13 01:20:39 +0900

    :sparkles: #24 - yml 수정 및 로그인 페이지를 제공 해주는 getLoginPageUrl 정의

---
+++ src/main/kotlin/com/teamsparta/jobtopia/infra/security/oauth/KakaoOAuthClient.kt
@@ -0,0 +1,27 @@
+package com.teamsparta.jobtopia.infra.security.oauth
+
+import org.springframework.beans.factory.annotation.Value
+
+class KakaoOAuthClient(
+    @Value("\${oauth2.kakao.client_id}") val clientId : String,
+    @Value("\${oauth2.kakao.redirect_url}") val redirectUrl : String,
+    @Value("\${oauth2.kakao.auth_server_base_url}") val authServerBaseUrl : String,
+    @Value("\${oauth2.kakao.resource_server_base_url}") val resourceServerBaseUrl : String
+): OAuthClient {
+    override fun getLoginPageUrl(): String {
+        return StringBuilder(authServerBaseUrl)
+            .append("/oauth/authorize")
+            .append("?client_id=").append(clientId)
+            .append("&redirect_uri=").append(redirectUrl)
+            .append("&response_type)=").append("code")
+            .toString()
+    }
+
+    override fun getAccessToken(code: String): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUserInfo(accessToken: String): OAuthUserInfoResponse {
+        TODO("Not yet implemented")
+    }
+}
