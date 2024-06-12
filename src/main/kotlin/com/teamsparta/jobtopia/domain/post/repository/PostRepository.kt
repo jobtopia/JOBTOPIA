2a5fb17c66dc876fd86f8db43f9431a07e7560a1
gooddle <vptl6559@gmail.com>
2024-06-12 19:12:25 +0900

    :zap: #20 패키치 위치 이동 및 레포지토리 수정

---
+++ src/main/kotlin/com/teamsparta/jobtopia/domain/post/repository/PostRepository.kt
@@ -0,0 +1,12 @@
+package com.teamsparta.jobtopia.domain.post.repository
+
+import com.teamsparta.jobtopia.domain.post.model.Post
+
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
+
+
+interface  PostRepository: JpaRepository<Post, Long> {
+    fun findByDeletedFalse(pageable: Pageable): Page<Post>
+}
