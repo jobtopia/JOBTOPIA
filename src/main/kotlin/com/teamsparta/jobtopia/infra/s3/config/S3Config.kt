b863771e652af2df9d4e1d8fdab0f102b4eae5c1
DanDanjoo <murekal94@gmail.com>
2024-06-14 17:33:29 +0900

    :wrench: #27 - S3Config 수정

--- src/main/kotlin/com/teamsparta/jobtopia/infra/s3/config/S3Config.kt
+++ src/main/kotlin/com/teamsparta/jobtopia/infra/s3/config/S3Config.kt
@@ -1,8 +1,9 @@
 package com.teamsparta.jobtopia.infra.s3.config
 
+import com.amazonaws.auth.AWSCredentials
 import com.amazonaws.auth.AWSStaticCredentialsProvider
+
 import com.amazonaws.auth.BasicAWSCredentials
-import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import org.springframework.beans.factory.annotation.Value
@@ -12,17 +13,17 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class S3Config(
-    @Value("\${cloud.aws.credentials.accessKey}") val accessKey : String,
-    @Value("\${cloud.aws.credentials.secretKey}") val secretKey : String,
+    @Value("\${cloud.aws.credentials.accessKey}") private val accessKey : String? = null,
+    @Value("\${cloud.aws.credentials.secretKey}") private val secretKey : String? = null,
+    @Value("\${cloud.aws.region}") private val region : String? = null
 ) {
 
     @Bean
     fun amazonS3Client(): AmazonS3 {
+        val credentials : AWSCredentials = BasicAWSCredentials(accessKey, secretKey)
         return AmazonS3ClientBuilder.standard()
-            .withCredentials(
-                AWSStaticCredentialsProvider(BasicAWSCredentials(accessKey, secretKey))
-            )
-            .withRegion(Regions.AP_NORTHEAST_2)
+            .withRegion(region)
+            .withCredentials(AWSStaticCredentialsProvider(credentials))
             .build()
     }
 }
