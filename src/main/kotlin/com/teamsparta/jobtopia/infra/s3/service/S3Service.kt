3a6ffe64defe7d9d78896e98bec5ffe694c1d7dd
DanDanjoo <murekal94@gmail.com>
2024-06-14 17:38:55 +0900

    :wrench: #27 - S3Service 수정

--- src/main/kotlin/com/teamsparta/jobtopia/infra/s3/service/S3Service.kt
+++ src/main/kotlin/com/teamsparta/jobtopia/infra/s3/service/S3Service.kt
@@ -1,7 +1,48 @@
 package com.teamsparta.jobtopia.infra.s3.service
 
+
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.model.CannedAccessControlList
+import com.amazonaws.services.s3.model.ObjectMetadata
+import com.amazonaws.services.s3.model.PutObjectRequest
+import com.amazonaws.util.IOUtils
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.PropertySource
 import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import java.io.ByteArrayInputStream
+import java.io.IOException
+import java.util.*
 
 
+@PropertySource("classpath:aws.yml")
 @Service
-class S3Service() {}
+class S3Service(
+    private val amazonS3Client: AmazonS3
+) {
+
+    @Value("\${bucket}")
+    lateinit var bucket: String
+
+    @Value("\${dir}")
+    lateinit var dir: String
+
+    @Throws(IOException::class)
+    fun upload(file: MultipartFile): String {
+        val fileName = UUID.randomUUID().toString() + "-" + file.originalFilename
+        val objMeta = ObjectMetadata()
+
+        val bytes = IOUtils.toByteArray(file.inputStream)
+        objMeta.contentLength = bytes.size.toLong()
+
+        val byteArrayIs = ByteArrayInputStream(bytes)
+
+        amazonS3Client.putObject(
+            PutObjectRequest(bucket, dir + fileName, byteArrayIs, objMeta)
+                .withCannedAcl(CannedAccessControlList.PublicRead))
+
+        return amazonS3Client.getUrl(bucket, dir + fileName).toString()
+    }
+
+}
