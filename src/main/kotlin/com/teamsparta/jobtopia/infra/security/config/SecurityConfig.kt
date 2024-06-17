f00d2bca334d9df3beca41de0389c2b573e1f796
gooddle <vptl6559@gmail.com>
2024-06-17 22:21:56 +0900

    :zap: #47 @EnableMethodSecurity 추가

--- src/main/kotlin/com/teamsparta/jobtopia/infra/security/config/SecurityConfig.kt
+++ src/main/kotlin/com/teamsparta/jobtopia/infra/security/config/SecurityConfig.kt
@@ -4,6 +4,7 @@ import com.teamsparta.jobtopia.infra.security.jwt.JwtAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.AuthenticationEntryPoint
@@ -12,6 +13,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 class SecurityConfig(
     private val jwtAuthenticationFilter: JwtAuthenticationFilter,
     private val authenticationEntrypoint: AuthenticationEntryPoint
